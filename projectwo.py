20d233b11775ec92cdabf0e8754fb595039b8be1
Khang8-1 <cis1700019@students.cis.edu.vn>
2025-10-22 09:06:00 +0700

    Add files via upload

---
+++ projectwo.py
@@ -0,0 +1,11 @@
+numbers_list = []
+num1 = int(input("Enter a number: "))
+numbers_list.append(num1)
+
+num2 = int(input("Enter a number: "))
+numbers_list.append(num2)
+
+num3 = int(input("Enter a number: "))
+numbers_list.append(num3)
+numbers_list.sort()
+print(numbers_list)
