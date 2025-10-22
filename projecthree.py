812482d662ce1550f1b70327887cb1c431ddcd32
Khang8-1 <cis1700019@students.cis.edu.vn>
2025-10-22 09:16:37 +0700

    Add files via upload

---
+++ projecthree.py
@@ -0,0 +1,8 @@
+
+list = [10, 5, 2, 8, 4]  # Example list with 5 integer elements
+list[0] = (list[0] * 2) - 5       # 10*2 - 5 = 15
+list[1] = (list[1] + 1) / 3       # (5+1)/3 = 2.0
+list[2] = (list[2] - 1) * 10      # (2-1)*10 = 10
+list[3] = (list[3] // 4) + 7      # (8//4) + 7 = 9
+list[4] = (list[4] * 3) + list[0] # (4*3) + 15 = 27
+print(list)
